efbd6341c427599ef6cb74fca592e022faa72237
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-20 15:13:12 -0500

    upload to github pages

--- src/components/Hotel/Hotel.js
+++ src/components/Hotel/Hotel.js
@@ -24,26 +24,51 @@ export default function Hotel(){
                         'Close to de beach',
                         'Breakfast included',
                         'Free airport shuttle',
+                        'free wifi in all rooms',
                         'Air conditioning and heating',
                         'Pets allowed',
                         'We speak all languages',
                         'Perfect for families'
                     ]} />
-                    <div className='recommend__count'>
-                        <p className='paragraph'>
+                    <div className='recommend'>
+                        <p className='recommend__count'>
                             Lucy and other 3 friends recomend this hotel.
                         </p>
                         <div className='recommend__friends'>
-                            <img src='img/user-3.jpg' alt='friend 1' className='recommend_photo' />
-                            <img src='img/user-4.jpg' alt='friend 2' className='recommend_photo' />
-                            <img src='img/user-5.jpg' alt='friend 3' className='recommend_photo' />
-                            <img src='img/user-6.jpg' alt='friend 4' className='recommend_photo' />
+                            <img src='img/user-3.jpg' alt='friend 1' className='recommend__photo' />
+                            <img src='img/user-4.jpg' alt='friend 2' className='recommend__photo' />
+                            <img src='img/user-5.jpg' alt='friend 3' className='recommend__photo' />
+                            <img src='img/user-6.jpg' alt='friend 4' className='recommend__photo' />
                         </div>
                     </div>
                 </div>
                 <div className='user-reviews'>
-                    User reviews
+                    <Review 
+                    text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi, totam, itaque officia ex.'
+                    name='Nick Smith'
+                    foto='user-1.jpg'
+                    date='Feb 23rd, 2017'
+                    rate={7.8}
+                    />
+                    <Review 
+                    text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi.'
+                    name='Mary Thomas'
+                    foto='user-2.jpg'
+                    date='Sep 13th, 2017'
+                    rate={9.3}
+                    />
+
+                    <button className='btn-inline'>Show All <span>&rarr;</span></button>
                 </div>
+            </div>
+            <div className='cta'>
+                <h2 className='cta__book-now'>
+                    Good news! We have 4 free rooms for yout selected dates!
+                </h2>
+                <button className='btn'>
+                    <span className='btn__visible'>Book now</span>
+                    <span className='btn__invisible'>Only 4 rooms left</span>
+                </button>
             </div>
         </main>
     )
@@ -89,5 +114,25 @@ function Overview(props){
                 <div className='overview__rating-count'>{votes} votes</div>
             </div>
         </div>
+    )
+}
+
+function Review(props){
+    const {text,foto,name,date,rate} = props;
+
+    return(
+        <figure className='review'>
+            <blockquote className='review__text'>
+            {text}
+            </blockquote>
+            <figcaption className='review__user'>
+                <img src={`img/${foto}`} alt='User 1' className='review__photo' />
+                <div className='review__user-box'>
+                    <p className='review__user-name'>{name}</p>
+                    <p className='review__user-date'>{date}</p>
+                </div>
+                <div className='review__rating'>{rate}</div>
+            </figcaption>
+        </figure>
     )
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import './List.scss';
 export default function List(props){
     const {arr} = props;
 
-    const list = arr.map( ar => <li className='list__item'>{ar}</li>);
+    const list = arr.map( (ar,i) => <li className='list__item' key={i}>{ar}</li>);
 
     return(
         <ul className='list'>
