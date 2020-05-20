aec87223f6fe8267480a01d9ff1a57dd3cc4e449
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-20 15:14:10 -0500

    Updates

---
+++ precache-manifest.438692e1f7255819f272206e5014240c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b504f0e5ffa2469efc3dbd6f372ef2cd",
+    "url": "/trillo/index.html"
+  },
+  {
+    "revision": "254ed650f4ceb291371c",
+    "url": "/trillo/static/css/main.5aa7dafa.chunk.css"
+  },
+  {
+    "revision": "06958d5ec5757bfa5ee5",
+    "url": "/trillo/static/js/2.30b57d8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trillo/static/js/2.30b57d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254ed650f4ceb291371c",
+    "url": "/trillo/static/js/main.a6ea2c2c.chunk.js"
+  },
+  {
+    "revision": "b648c1a1d9ffb0ed9975",
+    "url": "/trillo/static/js/runtime-main.007b45bf.js"
+  }
+]);
